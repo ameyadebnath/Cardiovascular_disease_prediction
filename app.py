dd2cebce573b4088b991e5b0a587d4415b5e9f37
ameya <52748765+ameyadebnath@users.noreply.github.com>
2023-01-20 20:01:06 +0600

    Add files via upload
    Initial commit_04

---
+++ app.py
@@ -0,0 +1,83 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+# Load the logistic regression model
+filename = 'classifier.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+
+# Load the standardise data
+filename2 = 'scaler.pkl'
+scaler = pickle.load(open(filename2, 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        gender = request.form['gender']
+        height = int(request.form['height'])
+        weight = int(request.form['weight'])
+        ap_hi = int(request.form['ap_hi'])
+        ap_lo = int(request.form['ap_lo'])
+        cholesterol = request.form['cholesterol']
+        gluc = request.form['gluc']
+        smoke = request.form['smoke']
+        alco = request.form['alco']
+        active = request.form['active']
+
+        if gender == 'Female':
+            gender = 1
+        else:
+            gender = 2
+
+        if cholesterol == 'Normal':
+            cholesterol = 1
+        elif cholesterol == 'Above normal':
+            cholesterol = 2
+        else:
+            cholesterol = 3
+
+        if gluc == 'Normal':
+            gluc = 1
+        elif gluc == 'Above Normal':
+            gluc = 2
+        else:
+            gluc = 3
+
+        if smoke == 'Yes':
+            smoke = 1
+        else:
+            smoke = 0
+
+        if alco == 'Yes':
+            alco = 1
+        else:
+            alco = 0
+
+        if active == 'Yes':
+            active = 1
+        else:
+            active = 0
+
+        data = np.array([[age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active]])
+
+        data_s = scaler.transform(data)
+
+        my_prediction = classifier.predict(data_s)
+        return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,79 @@
+#importing packages
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+import sklearn.metrics
+import pickle
+
+#importing the csv file
+data = pd.read_csv('G:/Project/AI/cardio_train.csv', delimiter=';')
+
+data.head()
+
+#Data modification
+#1. Age - Converting age into years
+data['age']=data['age']/365
+
+#Checking for any missing value
+data.isnull().sum()
+
+X = data.drop(['cardio', 'id'], axis=1)
+y = data['cardio']
+
+#Train test split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print('Shape of x_train: ', x_train.shape)
+print('Shape of x_test: ', x_test.shape)
+print('Shape of y_train: ', y_train.shape)
+print('Shape of y_test: ', y_test.shape)
+
+#Standardising data
+scaler = StandardScaler()
+x_train_s = scaler.fit_transform(x_train)
+x_test_s = scaler.transform(x_test)
+
+#save standardise as pickle
+pickle.dump(scaler, open("scaler.pkl", 'wb'))
+
+gamma = [0.01, 0.1, 1, 10, 100]
+weight = [None, 'balanced']
+param_distributions = dict(C=gamma, class_weight=weight)
+print(param_distributions)
+
+# instantiate and fit the grid
+grid = RandomizedSearchCV(LogisticRegression(penalty='l2'), param_distributions, cv=5, scoring='f1', return_train_score=False, n_jobs=-1)
+
+grid.fit(x_train_s, y_train)
+
+# examine the best model
+print(grid.best_score_)
+print(grid.best_params_)
+
+LR_optimal=LogisticRegression(penalty='l2', C=100, class_weight='balanced')
+
+# fitting the model
+LR_optimal.fit(x_train_s, y_train)
+
+#train
+acc_log_train = round(LR_optimal.score(x_train_s, y_train) * 100, 2)
+
+#test
+acc_log_test = round(LR_optimal.score(x_test_s, y_test) * 100, 2)
+
+
+acc_log_train
+
+acc_log_test
+
+
+#save model as pickle
+pickle.dump(LR_optimal, open("classifier.pkl", "wb"))
